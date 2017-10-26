71fe5bee161b8600b174cc5d80aa9387cec801b7
artemovvictor <33093419+artemovvictor@users.noreply.github.com>
2017-10-26 13:58:23 +0300

    Вариант решения домашнего задания №1.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,85 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume r = null;
+        try {
+            for (int i = 0; i < storage.length; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    r = storage[i];
+                    break;
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println(uuid + " в базе отсутствует.");
+        }
+        return r;
     }
 
     void delete(String uuid) {
+        try {
+            for (int i = 0; i < storage.length; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    storage[i] = null;
+
+                    /* Оптимизация массива сортировкой*/
+                    for (int j = i; j < storage.length-1; j++) {
+                        if (storage[j] == null && storage[j + 1] != null) {
+                            storage[j] = storage[j + 1];
+                            storage[j + 1] = null;
+                        }
+                    }
+                    break;
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println(uuid + " удалить невозможно, в базе отсутствует.");
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] notNull = new Resume[size()];
+
+        for (int i = 0, j = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                notNull[j] = storage[i];
+                j++;
+            }
+        }
+        return notNull;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
