3ee16d5051af3455a874dc276e53ebcaf6e5bca6
artemovvictor <33093419+artemovvictor@users.noreply.github.com>
2017-10-30 10:50:53 +0300

    Вариант решения домашнего задания №1.1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,56 +5,53 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
+    private int resCount = 0;
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < resCount; i++) {
             storage[i] = null;
         }
+        resCount = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
-        }
+        storage[resCount] = r;
+        resCount++;
     }
 
     Resume get(String uuid) {
         Resume r = null;
-        try {
-            for (int i = 0; i < storage.length; i++) {
+            for (int i = 0; i < resCount; i++) {
                 if (storage[i].uuid.equals(uuid)) {
                     r = storage[i];
                     break;
                 }
+                else if (i >= resCount-1) {
+                    System.out.println("Резюме в базе отсутствует.");
+                }
             }
-        } catch (NullPointerException e) {
-            System.out.println(uuid + " в базе отсутствует.");
-        }
         return r;
     }
 
     void delete(String uuid) {
-        try {
-            for (int i = 0; i < storage.length; i++) {
+            for (int i = 0; i < resCount; i++) {
                 if (storage[i].uuid.equals(uuid)) {
                     storage[i] = null;
 
                     /* Оптимизация массива сортировкой*/
-                    for (int j = i; j < storage.length-1; j++) {
+                    for (int j = i; j < resCount-1; j++) {
                         if (storage[j] == null && storage[j + 1] != null) {
                             storage[j] = storage[j + 1];
                             storage[j + 1] = null;
                         }
                     }
+                    resCount--;
                     break;
                 }
+                else if (i >= resCount-1) {
+                    System.out.println(uuid + " удалить невозможно, в базе отсутствует.");
+                }
             }
-        } catch (NullPointerException e) {
-            System.out.println(uuid + " удалить невозможно, в базе отсутствует.");
-        }
 
     }
 
@@ -62,9 +59,9 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] notNull = new Resume[size()];
+        Resume[] notNull = new Resume[resCount];
 
-        for (int i = 0, j = 0; i < storage.length; i++) {
+        for (int i = 0, j = 0; i < resCount; i++) {
             if (storage[i] != null) {
                 notNull[j] = storage[i];
                 j++;
@@ -74,12 +71,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int count = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                count++;
-            }
-        }
-        return count;
+        return resCount;
     }
 }
